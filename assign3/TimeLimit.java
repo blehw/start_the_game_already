0adf640ff688563fe54934a5d698b825ed710e65
jpknwls <jkn0wles@stanford.edu>
2018-04-20 10:06:02 -0700

    Create TimeLimit.java

---
+++ assign3/TimeLimit.java
@@ -0,0 +1,91 @@
+package Assignment3;
+
+import java.util.List;
+
+import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+
+public class minimax_time extends SampleGamer {
+
+	long timeLimit;
+
+	private int maxScore(Role role, MachineState state) throws GoalDefinitionException, TransitionDefinitionException, MoveDefinitionException {
+
+		if (getStateMachine().findTerminalp(state)) {
+			return getStateMachine().findReward(role, state);
+		}
+
+		List<Move> moves = getStateMachine().getLegalMoves(state, role);
+		int score = 0;
+		for (int i = 0; i < moves.size(); i++) {
+			if (System.currentTimeMillis() > timeLimit) { return 0; }
+			int result = minScore(role, moves.get(i), state);
+			if (result > score) {
+				score = result;
+			}
+		}
+		return score;
+	}
+
+	private int minScore(Role role, Move move, MachineState state) throws GoalDefinitionException, TransitionDefinitionException, MoveDefinitionException {
+		List<List<Move>> jointMoves = getStateMachine().getLegalJointMoves(state, role, move);
+		int score = 100;
+		for (int i = 0; i < jointMoves.size(); i++) {
+			if (System.currentTimeMillis() > timeLimit) { return 0; }
+			MachineState newState = getStateMachine().getNextState(state, jointMoves.get(i));
+			int result = maxScore(role, newState);
+			if (result < score) {
+				score = result;
+			}
+		}
+		return score;
+	}
+
+	private Move bestMove(Role role, MachineState state) throws GoalDefinitionException, TransitionDefinitionException, MoveDefinitionException {
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		Move action = moves.get(0);
+		int score = 0;
+		for (int i = 0; i < moves.size(); i++) {
+			int result = minScore(role, moves.get(i), state);
+			if (result > score) {
+				score = result;
+				action = moves.get(i);
+			}
+		}
+		return action;
+	}
+
+	/**
+	 * This function is called at the start of each round
+	 * You are required to return the Move your player will play
+	 * before the timeout.
+	 *
+	 */
+	@Override
+	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		// We get the current start time
+		//long start = System.currentTimeMillis();
+
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+
+		timeLimit = timeout - 1000;
+		Move selection = bestMove(getRole(), getCurrentState());
+
+		//long stop = System.currentTimeMillis();
+
+		/**
+		 * These are functions used by other parts of the GGP codebase
+		 * You shouldn't worry about them, just make sure that you have
+		 * moves, selection, stop and start defined in the same way as
+		 * this example, and copy-paste these two lines in your player
+		 */
+		//notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return selection;
+	}
+}
