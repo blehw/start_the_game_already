53760cef0bba7bfaa51e030f15b2dcfe9dc262d1
jpknwls <jkn0wles@stanford.edu>
2018-04-30 09:11:24 -0700

    Add files via upload

---
+++ assignment4/MCTSNode.java
@@ -0,0 +1,63 @@
+package Assignment4;
+
+import java.util.ArrayList;
+
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+
+
+public class MCTSNode {
+	public double score;
+	public int visits;
+	public Move move;
+	public MachineState  state;
+	public ArrayList<MCTSNode> children;
+	public MCTSNode parent;
+
+	public MCTSNode(MCTSNode parent) {
+	      this.parent = parent;
+	      this.visits = 0;
+	      this.score = 0;
+	    }
+
+
+	public MCTSNode(MCTSNode parent, Move move, MachineState  state) {
+	      this.parent = parent;
+	      this.visits = 0;
+	      this.move = move;
+	      this.state = state;
+	      this.score = 0;
+	    }
+
+	private double selectfn(MCTSNode node) {
+		return node.score/node.visits
+		+ Math.sqrt(2*Math.log(node.parent.visits)/node.visits);
+
+	}
+
+
+	public MCTSNode selectChild(MCTSNode node) {
+		int score = 0;
+		MCTSNode select = node.children.get(0);
+		for(MCTSNode child : node.children) {
+			if (child.visits == 0) {
+				return child;
+			}
+			if (selectfn(child) > score) {
+				select = child;
+			}
+		}
+		return select;
+	}
+
+
+	public void backpropagate(MCTSNode node, double score) {
+		if (this.parent == null) { return; }
+		this.score += score;
+		this.visits++;
+		backpropagate(node.parent, score);
+	}
+
+
+
+}
---
+++ assignment4/MonteCarloTreeSearch.java
@@ -0,0 +1,151 @@
+package Assignment4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import org.ggp.base.player.gamer.event.GamerSelectedMoveEvent;
+import org.ggp.base.player.gamer.statemachine.sample.SampleGamer;
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+import org.ggp.base.util.statemachine.Role;
+import org.ggp.base.util.statemachine.exceptions.GoalDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.MoveDefinitionException;
+import org.ggp.base.util.statemachine.exceptions.TransitionDefinitionException;
+
+public class MonteCarloTreeSearch extends SampleGamer {
+	final int probes = 4;
+	final int maxLevel = 1;
+
+	private MCTSNode root = new MCTSNode(null);
+
+	private Move bestMove(Role role, MachineState state) throws GoalDefinitionException, TransitionDefinitionException, MoveDefinitionException {
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+		Move action = moves.get(0);
+		int score = 0;
+
+		if(moves.size() == 1)
+			return moves.get(0);
+
+		for (int i = 0; i < moves.size(); i++) {
+			int result = minScore(role, moves.get(i), state, 0);
+			if (result > score) {
+				score = result;
+				action = moves.get(i);
+			}
+		}
+		return action;
+	}
+
+	private int maxScore(Role role, MachineState state, int level) throws GoalDefinitionException, TransitionDefinitionException, MoveDefinitionException {
+
+		if (getStateMachine().findTerminalp(state))
+			return getStateMachine().getGoal(state, role);
+
+		if(level > maxLevel)
+			return monteCarlo(role, state, probes);
+
+
+		List<Move> moves = getStateMachine().getLegalMoves(state, role);
+		int score = 0;
+
+		for (int i = 0; i < moves.size(); i++) {
+			int result = minScore(role, moves.get(i), state, level+1);
+			if (result == 100)
+				return result;
+			if (result > score)
+				score = result;
+		}
+		return score;
+	}
+
+	private int minScore(Role role, Move move, MachineState state, int level) throws GoalDefinitionException, TransitionDefinitionException, MoveDefinitionException {
+		List<List<Move>> jointMoves = getStateMachine().getLegalJointMoves(state, role, move);
+		int score = 100;
+		for (int i = 0; i < jointMoves.size(); i++) {
+			MachineState newState = getStateMachine().getNextState(state, jointMoves.get(i));
+			int result = maxScore(role, newState, level);
+			if (result < score) {
+				score = result;
+			}
+		}
+		return score;
+	}
+
+
+	private void expandNode(MCTSNode node) throws MoveDefinitionException, TransitionDefinitionException {
+		List<List<Move>> jointMoves = getStateMachine().getLegalJointMoves(node.state, getRole(), node.move);
+		for(List<Move> move: jointMoves) {
+				MachineState newState = getStateMachine().getNextState(node.state, move);
+				MCTSNode child = new MCTSNode(node, move.get(0), newState);
+				node.children.add(child);
+		}
+	}
+
+	private void simulate(MCTSNode node, Role role) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException {
+		for (MCTSNode child: node.children) {
+			child.score = monteCarlo(role, child.state, probes);
+			child.backpropagate(child, child.score);
+		}
+	}
+
+
+
+
+	private int monteCarlo(Role role, MachineState state, int count) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException
+	{
+		int total = 0;
+		for(int i = 0; i<count; i++)
+		{
+			total += depthCharge(role, state);
+		}
+
+		return total/count;
+	}
+
+	private int depthCharge(Role role, MachineState state) throws GoalDefinitionException, MoveDefinitionException, TransitionDefinitionException
+	{
+		if (getStateMachine().findTerminalp(state))
+			return getStateMachine().getGoal(state, role);
+
+		List<Role> roles = getStateMachine().getRoles();
+		List<Move> moves = new ArrayList<Move>();;
+
+		for(int i = 0; i<roles.size(); i++)
+		{
+			List<Move> options = getStateMachine().getLegalMoves(state, roles.get(i));
+			moves.add(options.get(new Random().nextInt(options.size())));
+		}
+		MachineState newState = getStateMachine().getNextState(state, moves);
+		return depthCharge(role, newState);
+	}
+
+	/**
+	 * This function is called at the start of each round
+	 * You are required to return the Move your player will play
+	 * before the timeout.
+	 *
+	 */
+	@Override
+	public Move stateMachineSelectMove(long timeout) throws TransitionDefinitionException, MoveDefinitionException, GoalDefinitionException
+	{
+		// We get the current start time
+		long start = System.currentTimeMillis();
+
+		List<Move> moves = getStateMachine().getLegalMoves(getCurrentState(), getRole());
+
+		Move selection = bestMove(getRole(), getCurrentState());
+
+		long stop = System.currentTimeMillis();
+
+		/**
+		 * These are functions used by other parts of the GGP codebase
+		 * You shouldn't worry about them, just make sure that you have
+		 * moves, selection, stop and start defined in the same way as
+		 * this example, and copy-paste these two lines in your player
+		 */
+		notifyObservers(new GamerSelectedMoveEvent(moves, selection, stop - start));
+		return selection;
+	}
+
+}
