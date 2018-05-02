d94a724b89fd79f34aefad6e97b7c5f486a236a3
blehw <winton2095@gmail.com>
2018-05-01 18:37:53 -0700

    Add files via upload

---
+++ assignment4/winton/MCTSNode.java
@@ -0,0 +1,35 @@
+package Assignment4;
+
+import java.util.ArrayList;
+
+import org.ggp.base.util.statemachine.MachineState;
+import org.ggp.base.util.statemachine.Move;
+
+
+public class MCTSNode {
+	public double score;
+	public int visits;
+	public Move move;
+	public MachineState  state;
+	public ArrayList<MCTSNode> children;
+	public MCTSNode parent;
+	private final int C = 20;
+
+	public MCTSNode(MCTSNode parent) {
+		this.parent = parent;
+	    this.children = new ArrayList<MCTSNode>();
+	    this.visits = 0;
+	    this.score = 0;
+	}
+
+	public MCTSNode(MCTSNode parent, Move move, MachineState  state) {
+		this.parent = parent;
+	    this.children = new ArrayList<MCTSNode>();
+	    this.visits = 0;
+	    this.move = move;
+	    this.state = state;
+	    this.score = 0;
+	}
+}
+
+
